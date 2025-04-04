2514ba99bc81e5a3323858569f220d2191418eb2
therencho <109227339+therencho@users.noreply.github.com>
2025-04-04 22:40:33 +0000

    Revert

--- src/app/about/loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="loader">Loading...</div>
-    </div>
-  );
-};
-
-export default Loading;
--- src/app/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Premium Infotech - IT Solutions for Business Growth",
-  description: "Comprehensive IT solutions designed to optimize operations, enhance security, and accelerate growth in today's digital landscape.",
-}; 
